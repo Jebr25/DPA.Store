0132ffd216cdd489bf63aefacf22e82153366364
Jeber Joel Linares Cruz <Jeber.linares@gmail.com>
2023-09-23 21:33:32 -0500

    Se agrego los controladores de POST, GETBYID y DELETE

--- DPA.Store.API/Controllers/CategoryController.cs
+++ DPA.Store.API/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using DPA.Store.DOMAIN.Core.Interfaces;
+﻿using DPA.Store.DOMAIN.Core.Entities;
+using DPA.Store.DOMAIN.Core.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Client;
@@ -16,11 +17,38 @@ namespace DPA.Store.API.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        [HttpGet]
+        [HttpGet("GetAll")]
         public async Task<IActionResult> GetAll()
         {
             var categories = await _categoryRepository.GetAll();
             return Ok(categories);
+        }
+
+        [HttpGet("GetById/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var categories = await _categoryRepository.GetById(id);
+            return Ok(categories);
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(Category category)
+        {
+            var result = await _categoryRepository.Insert(category);
+            if (!result)
+                return BadRequest(result);
+            else
+                return Ok(result);
+        }
+
+        [HttpGet("Delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _categoryRepository.Delete(id);
+            if(!result)
+                return BadRequest(result);
+            else
+                return Ok(result);
         }
 
     }
