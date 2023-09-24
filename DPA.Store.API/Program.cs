a7112e1651c6f97acc834caf81c23bbf0aff6270
Jeber Joel Linares Cruz <Jeber.linares@gmail.com>
2023-09-23 20:38:59 -0500

    Se agrego el CategoryController

--- DPA.Store.API/Program.cs
+++ DPA.Store.API/Program.cs
@@ -1,4 +1,6 @@
+using DPA.Store.DOMAIN.Core.Interfaces;
 using DPA.Store.DOMAIN.Infrastructure.Data;
+using DPA.Store.DOMAIN.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 var _config = builder.Configuration;
 var _cnx = _config.GetConnectionString("DevConnection");
 builder.Services.AddDbContext<StoreDbContext>(options => { options.UseSqlServer(_cnx);});
+
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 
 
 builder.Services.AddControllers();
